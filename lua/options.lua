018a6054743e91ecc0481184c7d813689a3a11c3
SuharaDaigo <s8778@g.maizuru-ct.ac.jp>
2024-07-01 20:27:12 +0900

    環境整理

--- lua/options.lua
+++ lua/options.lua
@@ -1,53 +1,44 @@
-local options =
-{
   -- ファイル
-  fileencoding = "utf-8", -- UTF-8に設定
-  swapfile = false, 
-  hidden = true, -- バッファ切り替え時にファイルの保存不要
+  vim.opt.fileencoding = "utf-8" -- UTF-8に設定
+  vim.opt.swapfile = false 
+  vim.opt.hidden = true -- バッファ切り替え時にファイルの保存不要
 
   -- クリップボード共有
-  clipboard = 'unnamedplus',
+  vim.opt.clipboard = 'unnamedplus'
 
   -- メニューとコマンド
-  wildmenu = true, -- コマンドラインで補完
-  cmdheight = 1, -- コマンドラインの表示行数
-  laststatus = 2, -- 下部にステータスラインを表示
-  showcmd = true, -- コマンドラインに入力されたコマンドを表示
+  vim.opt.cmdheight = 1 -- コマンドラインの表示行数
+  vim.opt.laststatus = 2 -- 下部にステータスラインを表示
+  vim.opt.showcmd = true -- コマンドラインに入力されたコマンドを表示
 
   -- 検索・置換え
-  hlsearch = true, -- ハイライト検索を有効
-  incsearch = true, -- インクリメンタルサーチを有効
-  matchtime = 1, -- 入力された文字列がマッチするまでにかかる時間
+  vim.opt.hlsearch = true -- ハイライト検索を有効
+  vim.opt.incsearch = true -- インクリメンタルサーチを有効
+  vim.opt.matchtime = 1 -- 入力された文字列がマッチするまでにかかる時間
 
   -- カラースキーム
-  termguicolors = true, -- 24 ビットカラーを使用
-  background = "dark",
-  transparency = 10,
+  vim.opt.termguicolors = true -- 24 ビットカラーを使用
+  vim.opt.background = "dark"
 
   -- インデント
-  shiftwidth = 2, -- シフト幅を2に設定する
-  tabstop = 2, -- タブ幅を2に設定する
-  expandtab = true, -- タブ文字をスペースに置き換える
-  autoindent = true, -- 自動インデントを有効にする
-  smartindent = true, -- インデントを言語に合わせて調整する
+  vim.opt.shiftwidth = 2 -- シフト幅を2に設定する
+  vim.opt.tabstop = 2 -- タブ幅を2に設定する
+  vim.opt.expandtab = true -- タブ文字をスペースに置き換える
+  vim.opt.autoindent = true -- 自動インデントを有効にする
+  vim.opt.smartindent = true -- インデントを言語に合わせて調整する
 
   -- 表示
-  number = true, -- 行番号を表示
-  cursorline = true, -- 現在行をハイライト
-  relativenumber = false, -- 相対行番号を非表示
-  wrap = true, -- テキストの自動折り返しを有効に
-  showtabline = 2, -- タブラインを表示
-  visualbell = true, -- ビープ音を表示する代わりに画面をフラッシュ
-  showmatch = true, -- 対応する括弧をハイライト表示
-  list = true, -- 不可視文字を表示
-  scrolloff = 15, -- 上下から15行まで行ったら自動でスクロール
+  vim.opt.number = true -- 行番号を表示
+  vim.opt.cursorline = true -- 現在行をハイライト
+  vim.opt.relativenumber = false -- 相対行番号を非表示
+  vim.opt.wrap = true -- テキストの自動折り返しを有効に
+  vim.opt.showtabline = 2 -- タブラインを表示
+  vim.opt.visualbell = true -- ビープ音を表示する代わりに画面をフラッシュ
+  vim.opt.showmatch = true -- 対応する括弧をハイライト表示
+  vim.opt.list = true -- 不可視文字を表示
+  vim.opt.scrolloff = 15 -- 上下から15行まで行ったら自動でスクロール
 
   -- インタフェース
-  winblend = 0, -- ウィンドウの不透明度
-  pumblend = 100, -- ポップアップメニューの不透明度
-  signcolumn = "yes" -- 行数の横にサインカラムを表示
-}
-
-for k, v in pairs(options) do
-  vim.o[k] = v
-end
+  vim.opt.winblend = 0 -- ウィンドウの不透明度
+  vim.opt.pumblend = 100 -- ポップアップメニューの不透明度
+  vim.opt.signcolumn = "yes" -- 行数の横にサインカラムを表示
