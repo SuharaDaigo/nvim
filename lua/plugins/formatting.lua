d6aa34eb3882d8fd2ffa1e22cb71f0b00988bb78
SuharaDaigo <s8778@g.maizuru-ct.ac.jp>
2024-07-17 23:19:50 +0900

    modify formatting config

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -5,22 +5,28 @@ return {
     local conform = require("conform")
 
     conform.setup({
+      -- Map of filetype to formatters
       formatters_by_ft = {
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        svelte = { "prettier" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        graphql = { "prettier" },
-        liquid = { "prettier" },
         lua = { "stylua" },
-        python = { "isort", "black" },
+        -- Conform will run multiple formatters sequentially
+        go = { "goimports", "gofmt" },
+        -- Use a sub-list to run only the first available formatter
+        javascript = { { "prettierd", "prettier" } },
+        -- You can use a function here to determine the formatters dynamically
+        python = function(bufnr)
+          if require("conform").get_formatter_info("ruff_format", bufnr).available then
+            return { "ruff_format" }
+          else
+            return { "isort", "black" }
+          end
+        end,
+        -- Use the "*" filetype to run formatters on all filetypes.
+        ["*"] = { "codespell" },
+        -- Use the "_" filetype to run formatters on filetypes that don't
+        -- have other formatters configured.
+        ["_"] = { "trim_whitespace" },
       },
+
       format_on_save = {
         lsp_fallback = true,
         async = false,
