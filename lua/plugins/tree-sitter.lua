4160a36862cbd4a9d649f1724507f84ae5d14359
sDaigo1112 <s8778@g.maizuru-ct.ac.jp>
2024-08-27 01:00:49 +0900

    dart言語サーバ追加

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,31 +1,32 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = {"BufReadPost", "BufNewFile"},
-  build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
-  config = function()
-    require("nvim-treesitter.configs").setup {
-      highlight = { enable = true },
-      indent = { enable = true },
-      autotag = { enable = true },
-      ensure_installed = {
-        "json",
-        "html",
-        "css",
-        "javascript",
-        "typescript",
-        "markdown",
-        "markdown_inline",
-        "lua",
-        "vim",
-        "c",
-        "cpp",
-        "dockerfile",
-        "go",
-        "python",
-      },
-    }
-  end
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPost", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			highlight = { enable = true },
+			indent = { enable = true },
+			autotag = { enable = true },
+			ensure_installed = {
+				"c",
+				"cpp",
+				"css",
+				"dart",
+				"dockerfile",
+				"go",
+				"html",
+				"json",
+				"javascript",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"typescript",
+				"vim",
+			},
+		})
+	end,
 }
